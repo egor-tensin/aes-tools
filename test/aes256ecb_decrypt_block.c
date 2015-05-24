d84140f48f9180fce594ee77e288bd6bbfc11fc9
Egor Tensin <Egor.Tensin@gmail.com>
2015-05-25 02:40:04 +0300

    add test/
    Includes simple block encryption/decryption programs for the supported
    modes (AES128/192/256 ECB + AES256 CBC).

---
+++ test/aes256ecb_decrypt_block.c
@@ -0,0 +1,46 @@
+/**
+ * \file
+ * \author Egor Tensin <Egor.Tensin@gmail.com>
+ * \date 2015
+ * \copyright This file is licensed under the terms of the MIT License.
+ *            See LICENSE.txt for details.
+ */
+
+#include <aesni/all.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void exit_with_usage(const char* argv0)
+{
+    printf("Usage: %s KEY [CIPHER...]\n", argv0);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char** argv)
+{
+    __declspec(align(16)) AesBlock128 plain, cipher;
+    __declspec(align(16)) AesBlock256 key;
+
+    if (argc < 2)
+        exit_with_usage(argv[0]);
+
+    if (parse_aes_block256(&key, argv[1]) != 0)
+    {
+        fprintf(stderr, "Invalid 256-bit AES block '%s'\n", argv[1]);
+        exit_with_usage(argv[0]);
+    }
+
+    for (int i = 2; i < argc; ++i)
+    {
+        if (parse_aes_block128(&cipher, argv[i]) != 0)
+        {
+            fprintf(stderr, "Invalid 128-bit AES block '%s'\n", argv[i]);
+            continue;
+        }
+        plain = aes256ecb_decrypt(cipher, &key);
+        print_aes_block128(&plain);
+    }
+
+    return 0;
+}
