31eb9272bedd00476b6b109e282465940925d716
Egor Tensin <Egor.Tensin@gmail.com>
2015-06-04 08:05:49 +0300

    test: add test vectors from NIST 800-38A

---
+++ test/800-38a.py
@@ -0,0 +1,33 @@
+# Copyright 2015 Egor Tensin <Egor.Tensin@gmail.com>
+# This file is licensed under the terms of the MIT License.
+# See LICENSE.txt for details.
+
+if __name__ == '__main__':
+    import argparse, json, toolkit, sys
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', '-r', required=True,
+                        help='set path to *.exe files')
+    parser.add_argument('--sde', '-e', action='store_true',
+                        help='use Intel SDE to run *.exe files')
+    args = parser.parse_args()
+    tools = toolkit.Tools('C:\\build\\test\\Debug', use_sde=args.sde)
+    vectors = json.load(open('800-38a.json'))
+    success = True
+    for prefix in vectors:
+        if 'iv' in vectors[prefix]:
+            iv = vectors[prefix]['iv']
+            encrypt = lambda key, plaintexts: tools.encrypt_with_iv(prefix, key, iv, plaintexts)
+            decrypt = lambda key, ciphers: tools.decrypt_with_iv(prefix, key, iv, ciphers)
+        else:
+            encrypt = lambda key, plaintexts: tools.encrypt(prefix, key, plaintexts)
+            decrypt = lambda key, ciphers: tools.decrypt(prefix, key, ciphers)
+        key = vectors[prefix]['key']
+        plaintexts = vectors[prefix]['plaintexts']
+        ciphers = vectors[prefix]['ciphers']
+        success = success and tools.detect_mismatches(plaintexts, encrypt(key, plaintexts), ciphers)
+        success = success and tools.detect_mismatches(ciphers, decrypt(key, ciphers), plaintexts)
+    if success:
+        print('No mismatches detected!')
+    else:
+        print('Detected mismatches!')
+        sys.exit(1)
---
+++ test/toolkit.py
@@ -0,0 +1,62 @@
+# Copyright 2015 Egor Tensin <Egor.Tensin@gmail.com>
+# This file is licensed under the terms of the MIT License.
+# See LICENSE.txt for details.
+
+import os.path
+import subprocess
+import sys
+
+class Tools:
+    def __init__(self, root_dir_path, use_sde=False):
+        self._root_dir_path = root_dir_path
+        self._use_sde = use_sde
+
+    def _get_tool_path(self, tool_name):
+        return os.path.join(self._root_dir_path, tool_name)
+
+    def _get_encrypt_tool_path(self, prefix):
+        return self._get_tool_path('{0}_encrypt.exe'.format(prefix))
+
+    def _get_decrypt_tool_path(self, prefix):
+        return self._get_tool_path('{0}_decrypt.exe'.format(prefix))
+
+    def _capture_tool_output(self, tool_path, args):
+        with_sde = ['sde', '--', tool_path] if self._use_sde else [tool_path]
+        return subprocess.check_output(with_sde + args, universal_newlines=True).split()
+
+    def encrypt(self, prefix, key, args):
+        print('Encrypting using \'{0}\'...'.format(prefix))
+        print('\tKey:', key)
+        return self._capture_tool_output(self._get_encrypt_tool_path(prefix), [key] + args)
+
+    def decrypt(self, prefix, key, args):
+        print('Decrypting using \'{0}\'...'.format(prefix))
+        print('\tKey:', key)
+        return self._capture_tool_output(self._get_decrypt_tool_path(prefix), [key] + args)
+
+    def encrypt_with_iv(self, prefix, key, iv, args):
+        print('Encrypting using \'{0}\'...'.format(prefix))
+        print('\tKey:', key)
+        print('\tInitialization vector:', iv)
+        return self._capture_tool_output(self._get_encrypt_tool_path(prefix), [key, iv] + args)
+
+    def decrypt_with_iv(self, prefix, key, iv, args):
+        print('Decrypting using \'{0}\'...'.format(prefix))
+        print('\tKey:', key)
+        print('\tInitialization vector:', iv)
+        return self._capture_tool_output(self._get_decrypt_tool_path(prefix), [key, iv] + args)
+
+    def detect_mismatches(self, input, actual_output, expected_output):
+        if len(actual_output) != len(expected_output):
+            print('Unexpected output length!', file=sys.stderr)
+            print('\tExpected length:', len(expected_output), file=sys.stderr)
+            print('\tActual length:', len(actual_output), file=sys.stderr)
+            return False
+        for i in range(len(input)):
+            if actual_output[i] != expected_output[i]:
+                print('A mismatch detected!', file=sys.stderr)
+                print('\tInput:', input[i], file=sys.stderr)
+                print('\tExpected:', expected_output[i], file=sys.stderr)
+                print('\tActual:', actual_output[i], file=sys.stderr)
+                return False
+        return True
