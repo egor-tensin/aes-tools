9ded63c5f446e5fce741a97f3861131c1954e92f
Egor Tensin <Egor.Tensin@gmail.com>
2015-06-10 06:20:45 +0300

    code style

--- include/aesni/buffer.h
+++ include/aesni/buffer.h
@@ -10,11 +10,13 @@
 
 #include <stdio.h>
 
-size_t aes128ecb_encrypt_buffer(const unsigned char* src,
-                              size_t src_size,
-                              unsigned char* dest,
-                              Aes128KeySchedule* key_schedule);
-size_t aes128ecb_decrypt_buffer(const unsigned char* src,
-                              size_t src_size,
-                              unsigned char* dest,
-                              Aes128KeySchedule* inverted_schedule);
+size_t aes128ecb_encrypt_buffer(
+    const unsigned char* src,
+    size_t src_size,
+    unsigned char* dest,
+    Aes128KeySchedule* key_schedule);
+size_t aes128ecb_decrypt_buffer(
+    const unsigned char* src,
+    size_t src_size,
+    unsigned char* dest,
+    Aes128KeySchedule* inverted_schedule);
