92fe2f03af315d94508988d73b1a5fbcdbadddff
Egor Tensin <Egor.Tensin@gmail.com>
2017-01-22 08:47:41 +0300

    add all_test.bat

---
+++ test/all_test.bat
@@ -0,0 +1,15 @@
+@setlocal enabledelayedexpansion
+
+@if [%1] == [] goto exit_with_usage
+
+set utils_dir=%~1
+
+nist-sp-800-38a.py --path "%utils_dir%" || exit /b !errorlevel!
+cavp.py            --path "%utils_dir%" || exit /b !errorlevel!
+file.py            --path "%utils_dir%" || exit /b !errorlevel!
+
+@goto :eof
+
+:exit_with_usage
+@echo usage: %0 UTILS_DIR >&2
+@exit /b 1
