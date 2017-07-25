6d76e1620a3369572b17e0bb2c4f223df8ac49f0
Egor Tensin <Egor.Tensin@gmail.com>
2017-07-26 01:38:30 +0200

    test/all.bat: code style

--- test/all.bat
+++ test/all.bat
@@ -1,8 +1,8 @@
 @setlocal enabledelayedexpansion
 
-@if [%1] == [] goto exit_with_usage
+@if "%~1" == "" goto exit_with_usage
 
-@set utils_dir=%~1
+@set "utils_dir=%~1"
 
 nist.py --path "%utils_dir%" || exit /b !errorlevel!
 cavp.py --path "%utils_dir%" || exit /b !errorlevel!
@@ -11,5 +11,5 @@ file.py --path "%utils_dir%" || exit /b !errorlevel!
 @goto :eof
 
 :exit_with_usage
-@echo usage: %0 UTILS_DIR >&2
+@echo usage: %~nx0 UTILS_DIR >&2
 @exit /b 1
