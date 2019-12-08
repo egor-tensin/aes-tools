20de645ad839d109bd6d2e8e5e1e600f84522dd5
Egor Tensin <Egor.Tensin@gmail.com>
2019-12-09 02:51:37 +0300

    AppVeyor: remove CMake options also set in common.cmake

--- .appveyor/build.ps1
+++ .appveyor/build.ps1
@@ -104,14 +104,11 @@ function Build-Project {
     mkdir $BuildDir
     cd $BuildDir
 
-    Invoke-Exe { cmake.exe -Wno-dev            `
+    Invoke-Exe { cmake.exe                     `
         -G $Generator -A $Platform             `
         -D "AES_TOOLS_ASM=$cmake_asm"          `
         -D "BOOST_ROOT=$BoostDir"              `
         -D "BOOST_LIBRARYDIR=$BoostLibraryDir" `
-        -D Boost_USE_STATIC_LIBS=ON            `
-        -D Boost_USE_STATIC_RUNTIME=ON         `
-        -D CXX_STANDARD=11                     `
         $ProjectDir
     }
     
