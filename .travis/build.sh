e82524a3ef90dd37732f6d5de1e448f8a9654224
Egor Tensin <Egor.Tensin@gmail.com>
2019-12-09 03:00:44 +0300

    Travis: use toolchains from cmake-common

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,23 +7,15 @@ readonly boost_fs="boost_${boost_version//\./_}"
 readonly boost_dir="$HOME/$boost_fs"
 readonly boost_librarydir="$boost_dir/stage/$arch/$build_type/lib"
 
-compiler_prefix=i686
-[ "$arch" = x64 ] && compiler_prefix=x86_64
-
-readonly cc="$compiler_prefix-w64-mingw32-gcc"
-readonly cxx="$compiler_prefix-w64-mingw32-g++"
-
 cd -- "$HOME"
 mkdir -- cmake
 cd -- cmake/
 
 cmake                                       \
     -D "CMAKE_BUILD_TYPE=$build_type"       \
-    -D "CMAKE_C_COMPILER=$cc"               \
-    -D "CMAKE_CXX_COMPILER=$cxx"            \
+    -D "CMAKE_TOOLCHAIN_FILE=$TRAVIS_BUILD_DIR/cmake/toolchains/mingw-w64-$arch.cmake" \
     -D "BOOST_ROOT=$boost_dir"              \
     -D "BOOST_LIBRARYDIR=$boost_librarydir" \
-    -D Boost_USE_STATIC_LIBS=ON             \
     -D CMAKE_SYSTEM_NAME=Windows            \
     "$TRAVIS_BUILD_DIR"
 
